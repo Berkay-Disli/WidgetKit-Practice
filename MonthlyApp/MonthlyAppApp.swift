5461112344140ad2034fb3dae648ec9f93b59e2c
Berkay-Disli <berkaydisli.work@gmail.com>
2022-09-20 17:46:40 +0300

    Initial Commit

---
+++ MonthlyApp/MonthlyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyAppApp.swift
+//  MonthlyApp
+//
+//  Created by Berkay Disli on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
